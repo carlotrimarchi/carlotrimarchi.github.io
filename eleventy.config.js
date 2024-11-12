f0579782f2819bcf9852446e8de9b357d6ab9331
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2024-11-12 14:36:16 +0100

    Add src directory

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,4 +2,10 @@ import pluginWebc from "@11ty/eleventy-plugin-webc";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebc);
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
 }
