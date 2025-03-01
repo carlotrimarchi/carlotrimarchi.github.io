1ae2303721871fa40a523659adcc24e52c49af4a
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2025-03-01 19:35:46 +0100

    Main layout ready

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,16 @@ import pluginWebc from "@11ty/eleventy-plugin-webc";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebc);
+  eleventyConfig.addPassthroughCopy({
+    "./src/assets/css": "./assets/css", // Ensure the correct path
+  });
+
+  const baseUrl =
+    process.env.ELEVENTY_ENV === "production" ? "/subdirectory" : "";
+
+  // Add baseUrl as global data to be used in templates
+  eleventyConfig.addGlobalData("baseUrl", baseUrl);
+
   return {
     dir: {
       input: "src",
---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+import postcssImportExtGlob from "postcss-import-ext-glob";
+import postcssImport from "postcss-import";
+
+export default {
+  plugins: [postcssImportExtGlob, postcssImport],
+};
