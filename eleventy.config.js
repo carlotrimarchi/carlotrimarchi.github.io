71a7025785572902689bcba31e47102df4461679
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2025-03-16 09:49:54 +0100

    First draft of new layout with new color

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,13 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 
 export default function (eleventyConfig) {
-  eleventyConfig.addPlugin(pluginWebc);
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: "src/_components/**/*.webc",
+  });
   eleventyConfig.addPassthroughCopy({
     "./src/assets/css": "./assets/css", // Ensure the correct path
   });
+  eleventyConfig.addPassthroughCopy("./src/assets/**/*.svg");
 
   const baseUrl =
     process.env.ELEVENTY_ENV === "production" ? "/subdirectory" : "";
