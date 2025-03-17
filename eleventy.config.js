f05e3110f76aee9329d030b30b1588e267725a56
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2025-03-17 18:01:12 +0100

    progress

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,12 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 
 export default function (eleventyConfig) {
+  eleventyConfig.addCollection("works", function (collectionApi) {
+    const result = collectionApi.getFilteredByGlob("src/work/*.md");
+    console.log(result);
+    return result;
+  });
+
   eleventyConfig.addPlugin(pluginWebc, {
     components: "src/_components/**/*.webc",
   });
